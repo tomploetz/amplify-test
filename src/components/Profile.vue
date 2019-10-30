475e506243eb2a068bb1ccc6986a614c05037e9f
Thomas Ploetz <tomploetz@Thomass-MBP.home>
2019-10-30 18:51:30 -0400

    initial auth example

---
+++ src/components/Profile.vue
@@ -0,0 +1,23 @@
+<template>
+  <h1>Welcome, {{user.username}}</h1>
+</template>
+
+<script>
+import { Auth } from 'aws-amplify'
+
+export default {
+  name: 'Profile',
+  data() {
+    return {
+      user: {}
+    }
+  },
+  beforeCreate() {
+    Auth.currentAuthenticatedUser()
+      .then(user => {
+        this.user = user
+      })
+      .catch(() => console.log('not signed in...'))
+  }
+}
+</script>
